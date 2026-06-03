6d18b657522efc7583dffe400c9dee2667b17611
sureshseervi-gamedev <suresh.seervi.d@gmail.com>
2026-06-03 18:42:08 +0530

    Revise robots function for metadata route

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,14 @@
-// import { MetadataRoute } from 'next'
-
-// export default function robots(): MetadataRoute.Robots {
-//   const baseUrl = 'https://augustopolonio.vercel.app'
+import { MetadataRoute } from 'next'
+export const dynamic = 'force-static'
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://augustopolonio.vercel.app'
   
-//   return {
-//     rules: {
-//       userAgent: '*',
-//       allow: '/',
-//       disallow: ['/api/', '/private/'], // Add paths you want to block from crawlers
-//     },
-//     sitemap: `${baseUrl}/sitemap.xml`,
-//   }
-// }
+  return {
+    rules: {
+    userAgent: '*',
+      allow: '/',
+      disallow: '/private/', // Add paths you want to block from crawlers
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
